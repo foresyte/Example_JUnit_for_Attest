05976cdcbd77e42a4bd3c605ee5de80d145fa7ce
Rich Szymczak <rich.szymczak@deque.com>
2018-05-03 15:01:18 -0400

    updated Jenkinsfile, is Jenkins pulling it down yet

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,4 +1,5 @@
 import com.deque.attest.AttestDriver;
+import com.deque.attest.matchers.selectors.IFrameSelector;
 import com.deque.attest.reporter.AttestReportingOptions;
 import org.junit.After;
 import org.junit.Before;
@@ -23,15 +24,25 @@ public class ExampleTest {
 
     AttestDriver page;
     private WebDriver webDriver;
+    AttestConfiguration attestConfig = AttestConfiguration.configure();
+    AttestReportingOptions arOptions = new AttestReportingOptions();
 
     @Before
     public void setUp() throws Exception {
 
-        AttestConfiguration.configure()
+        //attestConfig.forAuditSuite(auditConfiguration);
+
+        attestConfig
           .testSuiteName("Corporate Demo")
           .userAgent("Chrome")
-          .testMachine("Dev Machine")
+          .testMachine("RJS Macbook")
           .outputDirectory("/Users/richszymczak/repos/AttestIntegration/Example_JUnit");
+
+
+        //AttestReportingOptions aro = new AttestReportingOptions();
+        arOptions = arOptions.outputDirectory("/Users/richszymczak/repos/AttestIntegration/Example_JUnit");
+        arOptions = arOptions.testSuiteName("RJS Example Test");
+        arOptions = arOptions.uiState("production-home");
 
         System.setProperty("webdriver.chrome.driver", "/Users/richszymczak/repos/webdriver/chromedriver");
 
@@ -51,23 +62,108 @@ public class ExampleTest {
 
     @Test
     public void itShouldAuditPageForAccessibility() throws Exception {
-        assertThat(page, isAuditedForAccessibility());
+        arOptions.testSuiteName("Log generated from isAuditedForAccessibility()");
+        assertThat(page, isAuditedForAccessibility().logResults(arOptions));
 
     }
 
     @Test
     public void itShouldTestPageForAccessibility() throws Exception {
 
-        // RJS
-        AttestReportingOptions aro = new AttestReportingOptions();
-        aro = aro.outputDirectory("/Users/richszymczak/repos/AttestIntegration/Example_JUnit");
-        aro = aro.testSuiteName("RJS Example Test");
-        aro = aro.uiState("production-home");
-        assertThat(page, isAccessible().logResults(aro));
+        arOptions = arOptions.testSuiteName("RJS Example Test");
+
+        assertThat(page, isAccessible().logResults(arOptions));
+
+        // typically clicking through other pages, submitting a form and testing those scenarios
+        // go to DqU scan the login page, fill out the login form using the Selenium objects / abilities
+        // then scan the logged in page for accessibility issues
+
+        // a single test typically have multiple scans and log all of those results
+
+        // good practice - on a page, go through the interface, expose elements in different states in the app
+        // test that single element (e.g mega menu, modal popup,
+        // with Attest, instead of scanning whole page, utilize the API to tell me what has changed on the page
+        // and discover / scan new content / modal and scan that element and what's inside of it
+
+        // typically, these are tests that people already have, scenarios that we would leverage from their set of tests
+        // we consult them by showing them how to leverage existing tests with an Accessibiltiy point of view
+        // usually the customer has a bunch of tests and looking for how they can drop Attest into them
+
+
+
     }
 
     @Test
     public void itShouldTestPageWithOptions() throws Exception {
         assertThat(page, isAuditedForAccessibility().within("title").accordingTo("wcag2a"));
+        // for example, this is an API method like Tony is talking about
+
+        // make it able to scan multiple pages, each test have its own page (flexible)
+        // shows going out to different sites or different parts of same site
+        // understand -- what options are available with Attest
+         // -- API methods,
+        // custom rules and how they work
+         // integrations with Jenkins / SonarQube
     }
+
+    @Test
+    public void itShouldTestLaptopPage() throws Exception {
+
+        // navigate to another page, or try menu on page
+        String url = webDriver.getCurrentUrl();
+        webDriver.get(url + "/laptopsandnotebooks.php");
+        //Navigation nav = webDriver.navigate();
+        arOptions = arOptions.testSuiteName("RJS Laptop Page Test");
+        assertThat(page, isAccessible().logResults(arOptions));
+
+
+    }
+
+
+    @Test
+    public void itShouldTestShoppingCartPage() throws Exception {
+
+        // navigate to another page, or try menu on page
+        String url = webDriver.getCurrentUrl();
+        webDriver.get(url + "/cart.php");
+        arOptions = arOptions.testSuiteName("RJS Cart Page Test");
+
+        assertThat(page, isAuditedForAccessibility().within("title").accordingTo("wcag2a"));
+        assertThat(page, isAuditedForAccessibility().within("p").accordingTo("wcag2a"));
+        assertThat(page, isAccessible().within("table").accordingTo("wcag2a"));
+
+        assertThat(page, isAccessible().logResults(arOptions));
+
+
+    }
+
+    @Test
+    public void itShouldTestSomeLoginPage() throws Exception {
+        webDriver.get("https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=https%3A%2F%2Fwww.ebay.com%2F");
+
+        arOptions = arOptions.testSuiteName("RJS ebay Login Page Test");
+
+        //assertThat(page, isAccessible().within("table").accordingTo("wcag2a"));
+
+        assertThat(page, isAccessible().logResults(arOptions).within("table").accordingTo("wcag2a"));
+
+    }
+
+    @Test
+    public void itShouldTestDSGPage() throws Exception {
+
+        //webDriver.get("https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=https%3A%2F%2Fwww.ebay.com%2F");
+        webDriver.get("https://www.dickssportinggoods.com/OrderItemDisplay?calculationUsageId=-1&calculationUsageId=-2&calculationUsageId=-3&calculationUsageId=-7&updatePrices=1&doConfigurationValidation=Y&catalogId=12301&orderId=.&langId=-1&storeId=15108");
+
+        arOptions = arOptions.testSuiteName("RJS DSG Page Test");
+
+        //assertThat(page, isAccessible().within("table").accordingTo("wcag2a"));
+
+        assertThat(page, isAccessible().logResults(arOptions).within("table").accordingTo("wcag2a"));
+
+        // want to find a good example of a site to test that's using iframes
+        //assertThat(page, isAccessible().logResults(arOptions).within(new IFrameSelector("#frame1", "selector")));
+
+    }
+
 }
